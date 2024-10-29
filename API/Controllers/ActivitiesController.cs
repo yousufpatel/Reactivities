0625356fe02fa16f97c1e5bb15d42c7aca83eb58
yousuf.glb <yousuf.glb@gmail.com>
2024-10-29 11:22:14 +0530

    End of section 4

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,29 +1,43 @@
 using Domain;
+using Application.Activities;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Persistence;
 
 namespace API.Controllers
 {
     public class ActivitiesController : BaseApiController
     {
-        private readonly DataContext _context;
-        public ActivitiesController(DataContext context)
-        {
-            _context = context;
-
-        }
-
         [HttpGet] //api/activities
-        public async Task<ActionResult<List<Activity>>> GetActivities(){
-         
-             return await _context.Activities.ToListAsync();
+        public async Task<ActionResult<List<Activity>>> GetActivities()
+        {
+            return await Mediator.Send(new List.Query());
         }
 
         [HttpGet("{id}")] //api/activities/123
-        public async Task<ActionResult<Activity>> GetActivity(Guid id){
+        public async Task<ActionResult<Activity>> GetActivity(Guid id)
+        {
+            return await Mediator.Send(new Details.Query { Id = id });
+        }
 
-            return await _context.Activities.FindAsync(id);
+        [HttpPost]
+        public async Task<ActionResult> CreateActivity([FromBody] Activity activity)
+        {
+            await Mediator.Send(new Create.Command { Activity = activity });
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> EditActivity(Guid id, Activity activity)
+        {
+            activity.Id = id;
+            await Mediator.Send(new Edit.Command { Activity = activity });
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteActivity(Guid id)
+        {
+            await Mediator.Send(new Delete.Command { Id = id });
+            return Ok();
         }
     }
 }
--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,3 +1,4 @@
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
@@ -6,5 +7,7 @@ namespace API.Controllers
     [ApiController]
     public class BaseApiController : ControllerBase
     {
+        private IMediator _mediator;
+        protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
     }
 }
